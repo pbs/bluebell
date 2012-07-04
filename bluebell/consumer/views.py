989e8cd8880fff9e8903aae3fbd8c6e472d21a81
Furau Calin <calin.furau@gmail.com>
2012-07-04 12:10:07 +0300

    Created tables for the callsign data.

--- bluebell/consumer/views.py
+++ bluebell/consumer/views.py
@@ -14,12 +14,23 @@ def home(request):
 def localize_stations(request):
     sodor_base_url = settings.SODOR_ENDPOINT
     context = {}
+    callsigns = []
     if request.method == 'POST':
         zipcode = request.POST.get('zipcode')
         callsign_by_zip = settings.SODOR_CALLSIGN_BY_ZIP % zipcode
         url = urljoin(sodor_base_url, callsign_by_zip)
         data = json.loads(urllib.urlopen(url).read())
-        context['data'] = data
+        for item in data['$items']:
+            callsigns.append({
+                'callsign': item['$links'][0]['callsign'],
+                'short_common_name':
+                    item['$links'][0]['$links'][0]['short_common_name'],
+                'flagship':
+                    item['$links'][0]['$links'][0]['$links'][1]['callsign'],
+                'rank': item['rank'],
+                'confidence': item['confidence']}
+        )
+        context['data'] = callsigns
     return render_to_response(
         'localize_stations.html',
         context,
