e191d5185eac0286797eed92021204006170fcac
Furau Calin <calin.furau@gmail.com>
2012-07-04 17:35:41 +0300

    Forced input maxlength to five chars.

--- bluebell/consumer/extract.py
+++ bluebell/consumer/extract.py
@@ -3,13 +3,11 @@ import urllib
 import json
 
 
-def _read_data(url):
-    return json.loads(urllib.urlopen(url).read())
-
-
 def extract_callsign_by_zip(sodor_entry_point, zipcode):
         context = {}
         callsigns = []
+
+        context['zipcode'] = zipcode
 
         services_data = _read_data(sodor_entry_point)
         zipcode_collection_url = (
@@ -19,6 +17,9 @@ def extract_callsign_by_zip(sodor_entry_point, zipcode):
             re.sub('{zipcode}', zipcode, zipcode_collection_url)
         )
         zipcode_collection_data = _read_data(zipcode_collection_url)
+
+        if zipcode_collection_data['$items'] == []:
+            return context
 
         callsign_by_zip_url = (
             zipcode_collection_data['$items'][0]['$links'][0]['$self']
@@ -35,7 +36,10 @@ def extract_callsign_by_zip(sodor_entry_point, zipcode):
                 'rank': item['rank'],
                 'confidence': item['confidence']}
         )
-        context['zipcode'] = zipcode
         context['data'] = callsigns
 
         return context
+
+
+def _read_data(url):
+    return json.loads(urllib.urlopen(url).read())
