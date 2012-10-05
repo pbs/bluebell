ed29b4bb658d8cbea67daa044121a111ace74fd1
Edgar Roman <edgar@edgarroman.com>
2012-10-04 23:35:00 -0400

    updated flow to be by zip only

--- bluebell/consumer/urls.py
+++ bluebell/consumer/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 
 
-urlpatterns = patterns('bluebell',
-    url('^$', 'consumer.views.home'),
-    url('^localize_stations/$', 'consumer.views.localize_stations'),
-    url('^zipcode_listings/$', 'consumer.views.zipcode_listings'),
-    url('^episode_listings/$', 'consumer.views.episode_listings'),
-    url('^listings/(?P<callsign>\w+)/$', 'consumer.views.listings', name='listings'),
-    url('^stations/(?P<station_id>\d+)/$', 'consumer.views.view_station', name='view-station'),
-    url('^program/(?P<program_id>\d+)/(?P<callsign>\w+)/$', 'consumer.views.view_program', name='view-program'),
-    url('^show/(?P<show_id>\w+)/(?P<callsign>\w+)/$', 'consumer.views.view_show', name='view-show'),
+urlpatterns = patterns('bluebell.consumer.views',
+    url('^$', 'locator.home', name='home'),
+    url('^test?/$', 'locator.test', name='test'),
+    url('^station_by_zip/$', 'locator.station_by_zip', name='station_by_zip'),
+    url('^station_by_state/$', 'locator.station_by_state', name='station_by_state'),
+    url('^stations/(?P<station_id>\d+)/$', 'locator.view_station', name='view-station'),
+    url('^listings/(?P<callsign>\w+)/$', 'tvss.listings', name='listings'),
+    url('^program/(?P<program_id>\d+)/(?P<callsign>\w+)/$', 'tvss.view_program', name='view-program'),
+    url('^show/(?P<show_id>\w+)/(?P<callsign>\w+)/$', 'tvss.view_show', name='view-show'),
 )
