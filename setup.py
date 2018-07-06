0c2565f3ddadbc98184cd9f99adeed1c64d1b90e
James Turk <james.p.turk@gmail.com>
2018-07-06 11:13:10 -0400

    replace requirements.txt w/ setup.py, less redundancy

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup
 
 dependencies = [
     'django==1.4.1',
+    'python-dateutil==2.1',
+    'requests==0.13.9',
+    'resty==0.1',
+    'six==1.2.0',
 ]
 
 
 setup(
     name='bluebell',
-    version='0.1',
+    version='0.2',
     description='Sodor API consumer.',
     author='TPG CORE Services Team',
     author_email='tpg-pbs-coreservices@threepillarglobal.com',
