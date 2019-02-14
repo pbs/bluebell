fea884b2bec4611f92990d5f64d563739254f2f8
3PG-vrischitor <lucian.rischitor@3pillarglobal.com>
2019-02-14 10:00:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version='0.2',
     description='Sodor API consumer.',
     author='TPG CORE Services Team',
-    author_email='tpg-pbs-coreservices@threepillarglobal.com',
+    author_email='tpg-pbs-coreservices@3pillarglobal.com',
     url='https://github.com/pbs/bluebell',
     install_requires=dependencies,
 )
